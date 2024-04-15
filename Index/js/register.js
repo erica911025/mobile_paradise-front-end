44a8cd4655634dbc600de33976927054bcf769d2
s1411031013 <grong9913@gmail.com>
2024-04-15 17:42:56 +0800

    20240415

--- Index/js/register.js
+++ Index/js/register.js
@@ -1,12 +1,12 @@
 document.getElementById('registerForm').addEventListener('submit', function(event) {
     event.preventDefault(); // 阻止表單預設的提交行為
 
-   const formData = new FormData(this); // 獲取表單資料
+    const formData = new FormData(this); // 獲取表單資料
 
     // 將 FormData 轉換成 JSON 格式
     const jsonData = {};
     formData.forEach((value, key) => {
-        sonData[key] = value;
+        jsonData[key] = value;
     });
 
     const url = "http://localhost:5193/api/Member/Register"; // 替換成你的後端 API 的 URL
@@ -15,23 +15,23 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(jsonData), // 將表單資料轉換成 JSON 字串送出
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('伺服器回應錯誤');
-                }
-                return response.json(); // 解析回傳的 JSON
-            })
-            .then(data => {
-                // 在這裡處理從後端收到的回應
-                console.log(data);
-                // 可以在這裡添加一些反饋給使用者的動作，例如顯示註冊成功訊息或導向其他頁面
-                alert('註冊成功！');
-            })
-            .catch(error => {
-                // 處理錯誤
-                console.error('發生錯誤:', error);
-            });
-        });
+        },
+        body: JSON.stringify(jsonData), // 將表單資料轉換成 JSON 字串送出
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('伺服器回應錯誤');
+        }
+        return response.json(); // 解析回傳的 JSON
+    })
+    .then(data => {
+        // 在這裡處理從後端收到的回應
+        console.log(data);
+        // 可以在這裡添加一些反饋給使用者的動作，例如顯示註冊成功訊息或導向其他頁面
+        alert('註冊成功！');
+    })
+    .catch(error => {
+        // 處理錯誤
+        console.error('發生錯誤:', error);
+    });
+});
