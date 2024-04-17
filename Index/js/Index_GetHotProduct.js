eed5ab074cacdc252a66ea5440102a29caaaca5a
FangYu <ericayu911025@gmail.com>
2024-04-17 21:42:22 +0800

    0417

--- Index/js/Index_GetHotProduct.js
+++ Index/js/Index_GetHotProduct.js
@@ -1,7 +1,6 @@
-const url = "http://localhost:5193/api/Cart/Profile"; 
+const url = "http://localhost:5193/api/Paradise/Hot";
 
-
-fetch(url,{credentials: 'include'})
+fetch(url, { credentials: 'include' })
   .then(response => {
     if (!response.ok) {
       throw new Error('網路錯誤，無法獲取資料');
@@ -9,23 +8,39 @@ fetch(url,{credentials: 'include'})
     return response.json(); // 解析 JSON 格式的回傳資料
   })
   .then(data => {
+    console.log(data);
 
-    console.log(data); 
-    
-    const accountElement = document.getElementById('Account1');
-    const nameElement = document.getElementById('Name');
-    const cellphoneElement = document.getElementById('Cellphone');
-    const emailElement = document.getElementById('Email');
+    const carouselContainer = document.querySelector('.carousel-container .carousel');
 
-    
-    accountElement.textContent = data.account1;
-    nameElement.textContent = data.name;
-    cellphoneElement.textContent = data.cellphone;
-    emailElement.textContent = data.email;
-
+    if (data.Message && data.Message.length > 0) {
+      data.Message.forEach(item => {
+        const carouselItem = document.createElement('div');
+        carouselItem.classList.add('carousel-item');
+        carouselItem.innerHTML = `
+          <div class="product">
+            <img src="image/i15.png" alt="商品圖片"> 
+            <div class="text">
+              <a href="./product.html">
+                <div class="hot"> 
+                  <h4>【${item.Brand}】</h4>
+                </div>
+                <h3>${item.ItemName}</h3>
+                <div class="ItemPriceMin">
+                  <p>NT$</p>
+                  <p>${item.ItemPriceMin}起</p>
+                </div>
+              </a>                        
+            </div>                    
+          </div>
+        `;
+        carouselContainer.appendChild(carouselItem);
+      });
+    } else {
+      const noItemsMessage = document.createElement('div');
+      noItemsMessage.textContent = 'No items available';
+      carouselContainer.appendChild(noItemsMessage);
+    }
   })
   .catch(error => {
     console.error('發生錯誤:', error);
   });
-
-
