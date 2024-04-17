b072b90bac423d84438ac2e0f627631db3290bc3
FangYu <ericayu911025@gmail.com>
2024-04-17 15:19:44 +0800

    0417

--- Index/js/Product_Format.js
+++ Index/js/Product_Format.js
@@ -11,26 +11,6 @@ fetch(url, { credentials: 'include' })
   })
   .then(data => {
     console.log(data); 
-    
-    // 更新顏色選擇
-    const colorRadios = document.getElementsByName('color');
-    colorRadios.forEach(radio => {
-      if (radio.value === data.color) {
-        radio.checked = true;
-      } else {
-        radio.checked = false;
-      }
-    });
-
-    // 更新儲存空間選擇
-    const labelRadios = document.getElementsByName('label');
-    labelRadios.forEach(radio => {
-      if (radio.value === data.label) {
-        radio.checked = true;
-      } else {
-        radio.checked = false;
-      }
-    });
 
 
     // 更新商品品牌
@@ -66,7 +46,36 @@ fetch(url, { credentials: 'include' })
       ItemName2Element.textContent = 'No Brand Available';
     }
 
+    // 更新商品名稱價格
+    const ItemPriceElement = document.getElementById('ItemPrice');
+    if (data.Message && data.Message.length > 0) {
+      ItemPriceElement.textContent = data.Message[0].ItemPrice;
+      const itemPrice = parseFloat(data.Message[0].ItemPrice);
+    } else {
+      ItemPriceElement.textContent = 'No Brand Available';
+    }
+    
+
+     // 更新顏色選擇
+     const colorOptionsElement = document.getElementById('colorOptions');
+     if (data.Message && data.Message.length > 0) {
+       const Colors = data.Message.map(item => item.Color); //map可以抓取所有資料儲存於Colors陣列
+ 
+       Colors.forEach(Color => {
+         const label = document.createElement('label'); //創建顏色容器
+         label.id = Color.toLowerCase(); // 將顏色轉換為小寫作為 label 的 id
+         label.innerHTML = `
+           <input type="radio" name="color"  checked="checked">
+           <span class="round button">${Color}</span>
+         `;
+         colorOptionsElement.appendChild(label);
+       });
+     } else {
+       colorOptionsElement.textContent = 'No Colors Available';
+     }
+
     // 將商品規格顯示在對應的元素中
+    
     const colorElement = document.getElementById('Color');
     const spaceElement = document.getElementById('Space');
     const itemNumElement = document.getElementById('ItemNum');
@@ -80,34 +89,5 @@ fetch(url, { credentials: 'include' })
   .catch(error => {
     console.error('發生錯誤:', error);
   });
-
-
-//  fetch(url,{credentials: 'include'})
-//    .then(response => {
-//      if (!response.ok) {
-//        throw new Error('網路錯誤，無法獲取資料');
-//      }
-//      return response.json(); // 解析 JSON 格式的回傳資料
-//    })
-//    .then(data => {
- 
-//      console.log(data); 
-     
-//      const brandElement = document.getElementById('Brand'); //品牌
-//      const itemNameElement = document.getElementById('ItemName'); //名稱
-//      const colorElement = document.getElementById('Color'); //顏色
-//      const spaceElement = document.getElementById('Space'); //儲存空間
- 
-//      brandElement.textContent = data.brand;
-//      itemNameElement.textContent = data.itemName;
-//      colorElement.textContent = data.color;
-//      spaceElement.textContent = data.space;
-
- 
-//    })
-//    .catch(error => {
-//      console.error('發生錯誤:', error);
-//    });
- 
 
 
