e97e7b2449fd47d3797acfd09ef421129f78c798
s1411031013 <grong9913@gmail.com>
2024-04-24 17:06:45 +0800

    QQ

--- Index/js/Index_GetHotProduct.js
+++ Index/js/Index_GetHotProduct.js
@@ -45,3 +45,36 @@ fetch(url, { credentials: 'include' })
   .catch(error => {
     console.error('發生錯誤:', error);
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.querySelector('.carousel');
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    let currentIndex = 0;
+    const totalItems = carouselItems.length;
+    const itemsPerPage = 1; // 每頁顯示的商品數量
+
+    function showSlide(index) {
+        if (index < 0) {
+        currentIndex = totalItems - itemsPerPage;
+        } else if (index >= totalItems) {
+        currentIndex = 0;
+        } else {
+        currentIndex = index;
+        }
+
+        const offset = -currentIndex * (115 / itemsPerPage); // 將偏移量設置為正值
+        carousel.style.transform = `translateX(-${offset}%)`; // 使用負值來移動輪播圖片
+
+    }
+
+    prevBtn.addEventListener('click', function() {
+        showSlide(currentIndex + itemsPerPage);
+    });
+
+    nextBtn.addEventListener('click', function() {
+        showSlide(currentIndex - itemsPerPage);
+    });
+});
