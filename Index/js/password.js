8c75db1341600763bbb9686cef9e8aa075c6c2fb
FangYu <ericayu911025@gmail.com>
2024-04-29 16:30:11 +0800

    0429-9

--- Index/js/password.js
+++ Index/js/password.js
@@ -39,39 +39,39 @@ document.getElementById('ChangePasswordForm').addEventListener('submit', functio
         });
 
         function togglePasswordVisibility() {
-            var passwordInput = document.getElementsByName("OldPassword")[0];
-            var toggleIcon = document.getElementById("toggleIcon");
+            var passwordInput1 = document.getElementsByName("OldPassword")[0];
+            var toggleIcon = document.getElementById("toggleIcon1");
             
-            if (passwordInput.type === "OldPassword") {
-                passwordInput.type = "text";
+            if (passwordInput1.type === "password") {
+                passwordInput1.type = "text";
                 toggleIcon.src = "./image/view.png";
             } else {
-                passwordInput.type = "OldPassword";
+                passwordInput1.type = "password";
                 toggleIcon.src = "./image/hide.png";
             }
         }
 
         function togglePasswordVisibility2() {
-            var passwordInput = document.getElementsByName("NewPassword")[0];
-            var toggleIcon = document.getElementById("toggleIcon");
+            var passwordInput2 = document.getElementsByName("NewPassword")[0];
+            var toggleIcon = document.getElementById("toggleIcon2");
             
-            if (passwordInput.type === "NewPassword") {
-                passwordInput.type = "text";
+            if (passwordInput2.type === "password") {
+                passwordInput2.type = "text";
                 toggleIcon.src = "./image/view.png";
             } else {
-                passwordInput.type = "NewPassword";
+                passwordInput2.type = "password";
                 toggleIcon.src = "./image/hide.png";
             }
         }
         function togglePasswordVisibility3() {
-            var passwordInput = document.getElementsByName("NewPasswordCheck")[0];
-            var toggleIcon = document.getElementById("toggleIcon");
+            var passwordInput3 = document.getElementsByName("NewPasswordCheck")[0];
+            var toggleIcon = document.getElementById("toggleIcon3");
             
-            if (passwordInput.type === "NewPasswordCheck") {
-                passwordInput.type = "text";
+            if (passwordInput3.type === "password") {
+                passwordInput3.type = "text";
                 toggleIcon.src = "./image/view.png";
             } else {
-                passwordInput.type = "NewPasswordCheck";
+                passwordInput3.type = "password";
                 toggleIcon.src = "./image/hide.png";
             }
         }
