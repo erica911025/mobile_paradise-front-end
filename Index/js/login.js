ce074cf52bdba514f4948b574ea9d475d9d296e5
s1411031013 <grong9913@gmail.com>
2024-06-03 15:10:34 +0800

    停權

--- Index/js/login.js
+++ Index/js/login.js
@@ -27,7 +27,7 @@ form.addEventListener('submit', function(event) {
             window.location.href = './Index.html';
         } else {
             console.error('登入失敗', data.Message);
-            alert('登入失敗，請檢查您的帳號和密碼');
+            alert('登入失敗，帳號已被停權');
         }
     })
 
